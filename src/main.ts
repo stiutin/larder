e47018fa1e562038988f9bf02ed2603cbfa2a9d0
Serge <maestrelli1989@gmail.com>
2026-09-20 16:50:04 +0200

    upgrade Angular 20 -> 21

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-import {provideStore} from "@ngrx/store";
-import {provideEffects} from "@ngrx/effects";
-import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
-import {photoReducer} from "./app/store/reducer";
-import {PhotoEffects} from "./app/store/effects";
+import {provideZoneChangeDetection} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {provideRouter} from '@angular/router';
+import {provideHttpClient} from '@angular/common/http';
+import {provideStore} from '@ngrx/store';
+import {provideEffects} from '@ngrx/effects';
+import {AppComponent} from './app/app.component';
+import {routes} from './app/app.routes';
+import {photoReducer} from './app/store/reducer';
+import {PhotoEffects} from './app/store/effects';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideZoneChangeDetection(),
     provideRouter(routes),
-    provideStore({ photos: photoReducer }),
+    provideStore({photos: photoReducer}),
     provideEffects([PhotoEffects]),
-    provideHttpClient()
-  ]
+    provideHttpClient(),
+  ],
 });
